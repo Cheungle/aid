6793e1144fc27488412903dbf32c8f35bebb895e
Zkx198 <15121023105@163.com>
2020-05-22 15:28:08 +0800

    任务资源列表，新建有问题

---
+++ app/src/main/java/com/example/aid/ui/dashboard/DashboardNewTaskReources.java
@@ -0,0 +1,15 @@
+package com.example.aid.ui.dashboard;
+
+import android.os.Bundle;
+import com.example.aid.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardNewTaskReources extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_dashboard_new_task_resources);
+
+    }
+}

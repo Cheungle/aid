63f35547e37a81560ae5436f27890c573b2e8421
Zkx198 <15121023105@163.com>
2020-06-13 21:12:07 +0800

    新建任务

--- app/src/main/java/com/example/aid/ui/dashboard/DashboardNewTaskReources.java
+++ app/src/main/java/com/example/aid/ui/dashboard/DashboardNewTaskReources.java
@@ -15,16 +15,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.example.aid.R;
 import com.example.aid.data.DAL.DataBaseHelper;
+import com.example.aid.data.DAL.TaskDAL;
+import com.example.aid.data.DAL.UserDAL;
+import com.example.aid.data.model.identity;
 import com.example.aid.ui.login.LoginActivity;
+import com.example.aid.ui.notifications.identityEditActivity;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.Calendar;
 
@@ -36,12 +43,25 @@ public class DashboardNewTaskReources extends AppCompatActivity {
     private DatePickerDialog datePicker = null;
     private Button save_btn = null;
     private Button cancel_btn = null;
+    private String id;
+    private String TR_content,TR_place,TR_time;
+    private TaskDAL taskDAL=new TaskDAL(DashboardNewTaskReources.this);
+    private UserDAL userDAL=new UserDAL(DashboardNewTaskReources.this);
+    private RadioButton task_select;
+    private RadioButton resources_select;
+    private int type=0;
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_dashboard_new_task_resources);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        this.id = getIntent().getStringExtra("id");
+        identity iden = userDAL.selectIdentity(this.id);
+        final String identity_id = iden.getID();
+
+        new_time_btn = findViewById(R.id.new_time_btn);
         new_time_btn.setText(pubFun.format(calendar.getTime()));
 
         new_time_btn.setOnClickListener(new View.OnClickListener() {
@@ -50,12 +70,33 @@ public class DashboardNewTaskReources extends AppCompatActivity {
             }
         });
 
-        /*save_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                saveInfo();
-            }
-        });*/
+        task_select = (RadioButton)findViewById(R.id.rg_type_task);
+        resources_select = (RadioButton)findViewById(R.id.rg_type_resources);
 
+        save_btn = findViewById(R.id.new_TR_save_button);
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                EditText content = findViewById(R.id.new_TR_content_detail);
+                TR_content = content.getText().toString();
+                EditText place = findViewById(R.id.new_TR_location_detail);
+                TR_place = content.getText().toString();
+                TR_time = pubFun.format(calendar.getTime()).toString();
+
+                if (task_select.isChecked()){
+                    type=1;
+                }
+                if (resources_select.isChecked()){
+                    type=2;
+                }
+
+                try {
+                    saveInfo(identity_id);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        cancel_btn = findViewById(R.id.new_TR_cancel_button);
         cancel_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
                 exit();
@@ -91,7 +132,35 @@ public class DashboardNewTaskReources extends AppCompatActivity {
         finish();
     }
 
-    private void saveInfo() {
+    private void saveInfo(String identity_id) throws SQLException {
+        //Save之前判断用户是否进行了身份验证
+        //SharedPreferences sharedPreferences= getSharedPreferences("setting",Activity.MODE_PRIVATE);
+        //String userID =sharedPreferences.getString("userID", "");
+
+        //Log.i("info", "此次登录的用户是" + userID);
+
+        if(identity_id==null){
+            new AlertDialog.Builder(this)
+                    .setTitle("提示")
+                    .setMessage("您还未实名认证，请点击确定按钮进行认证！")
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            setResult(RESULT_OK);
+                            Intent intent=new Intent(DashboardNewTaskReources.this, identityEditActivity.class);
+                            DashboardNewTaskReources.this.startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            return;
+                        }
+                    })
+                    .show();
+        }else{
+            taskDAL.insertTask(id,TR_content,TR_time,type,TR_place);
+            Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
+
+        }
 
     }
 
